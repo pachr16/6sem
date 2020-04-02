64f4acec285e633e6cdc68fa02cd07ccd9908cc4
Pottemuld <32219655+Pottemuld@users.noreply.github.com>
2020-04-02 12:57:43 +0200

    Serverkode måske done

--- Streaming/server.js
+++ Streaming/server.js
@@ -1,17 +1,63 @@
-var http = require('http'),
-    fs   = require('fs'),
-    filePath = __dirname + '/bensound-buddy.mp3',
-    stat = fs.statSync(filePath);
+const http = require('http');
+const fileSystem = require('fs');
+const express = require('express');
+const ss = require('socket.io-stream');
+const path = require('path');
+const app = express();
+const api = express();
 
-http.createServer(function(request, response) {
+api.get('/getTrack', (req, res, err) => {
+  // generate file path
+  const filePath = path.resolve(__dirname, './html', './assets', req.query.songName + '.mp4');
+  // get file size info
+  const stat = fileSystem.statSync(filePath);
 
-    response.writeHead(200, {
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': stat.size ,
-        'Access-Control-Allow-Origin': '*'
-    });
+  // set response header info
+  res.writeHead(200, {
+    'Content-Type': 'audio/mpeg',
+    'Content-Length': stat.size
+  });
+  //create read stream
+  const readStream = fileSystem.createReadStream(filePath);
+  // attach this stream with response stream
+  readStream.pipe(res);
+});
 
-    // We replaced all the event handlers with a simple call to util.pump()
-    fs.createReadStream(filePath).pipe(response);
-})
-.listen(2000);
+//register api calls
+app.use('/api/v1/', api);
+
+/* send react app on / GET
+app.use(express.static(path.resolve(__dirname, './public/build/')));
+app.use(express.static(path.resolve(__dirname, './public/assets/')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public/build/', './index.html'));
+});
+*/
+
+const server = http.createServer(app);
+const io = require('socket.io').listen(server, {
+  log: false,
+  agent: false,
+  origins: '*:*',
+  transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
+});
+
+io.on('connection', client => {
+
+  const stream = ss.createStream();
+
+  client.on('getTrack', () => {
+    const filePath = path.resolve(__dirname, './html','./assets', req.query.songName + '.mp4');
+    const stat = fileSystem.statSync(filePath);
+    const readStream = fileSystem.createReadStream(filePath);
+    // pipe stream with response stream
+    readStream.pipe(stream);
+
+    ss(client).emit('track-stream', stream, { stat });
+  });
+  client.on('disconnect', () => {});
+});
+
+server.listen(process.env.PORT || '2000', function () {
+  console.log('Server app listening on port 2000!');
+});
