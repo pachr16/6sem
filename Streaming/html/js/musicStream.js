1fc99f0f97c5c00bee4c56f60da4877187d0804b
Rasmus Jensen <rasje17@student.sdu.dk>
2020-04-07 15:27:52 +0200

    pre react, worked on getting musicstream.js done (its not)
    decided to tryout react to solve the last issues we have wit props

--- Streaming/html/js/musicStream.js
+++ Streaming/html/js/musicStream.js
@@ -2,6 +2,18 @@ var socket = io('http://localhost:2000', { autoConnect: false });
 
 
 async function miscStartStream(songname) {
+    let source = null;
+    let playWhileLoadingDuration = 0;
+    let startAt = 0;
+    let audioBuffer = null;
+    let activeSource = null;
+
+    //from props find a solution!
+    const setDuration = 0;
+
+
+
+    const audioContext = getAudioContext();
     console.log("miscStartStream y'all!");
     socket.open();
     console.log("Howdy partner, socket is open for business!");
@@ -12,29 +24,49 @@ async function miscStartStream(songname) {
 
 
     //const loadFile = (onLoadProcess) =>
-        new Promise(async (resolve, reject) => {
-            socket.emit('streamSong', songname, () => { });
-            ss(socket).on('song-stream', (stream, { stat }) => {
-                stream.on('data', async (data) => {
-                    var audioContext = getAudioContext();
-                    const audioBufferChunk = await audioContext.decodeAudioData(withWaveHeader(data, 2, 44100));
-                    source = audioContext.createBufferSource();
-                    source.buffer = audioBufferChunk;
-                    source.connect(audioContext.destination);
-                    source.start();
 
+    return new Promise(async (resolve, reject) => {
+        socket.emit('streamSong', songname, () => { });
+        ss(socket).on('song-stream', (stream, { stat }) => {
 
-                    // calculate loading process rate
-                    const loadRate = (data.length * 100) / stat.size;
-                    onLoadProcess(loadRate);
+            // how far in the buffer are we
+            let rate = 0;
+            // is there data in the buffer
+            let isData = false;
 
-                    
-                   
+            stream.on('data', async (data) => {
 
-                    //const duration = (100/loadRate) * audioBufferChunk.duration;
-                });
-            });
+                const audioBufferChunk = await audioContext.decodeAudioData(withWaveHeader(data, 2, 44100));
+
+                const newaudioBuffer = (source && source.buffer)
+                    ? appendBuffer(source.buffer, audioBufferChunk, audioContext)
+                    : audioBufferChunk;
+                source = audioContext.createBufferSource();
+                source.buffer = newaudioBuffer;
+
+                // calculate loading process rate
+                const loadRate = (data.length * 100) / stat.size;
+                rate += loadRate
+
+                //changeAudionState is called here maby its a visualizer thingy?
+
+                if (rate >= 100) {
+                    clearInterval(whileLoadingInterval);
+                    audioBuffer = source.buffer;
+                    const inSec = (Date.now() - startAt) / 1000;
+                    activeSource.stop();
+                    play(inSec);
+                    resolve({ play, stop, setVolume });
+                }
+                isData = true;
+                // first time load
+                if (isData && rate === loadRate) {
+                    const duration = (100 / loadRate) * audioBufferChunk.duration;
+                    setDuration(duration)
+
+                };
         });
+    });
 
     /*
     socket.emit('playSong', 'beyonce')
@@ -54,9 +86,35 @@ async function miscStartStream(songname) {
     playing = true;
 }
 */
-    function getAudioContext() {
-        AudioContext = window.AudioContext || window.webkitAudioContext;    // for cross-browser support (?)
-        const audioContent = new AudioContext();
-        return audioContent;
-    }
+});
 }
+function getAudioContext() {
+    AudioContext = window.AudioContext || window.webkitAudioContext;    // for cross-browser support (?)
+    const audioContent = new AudioContext();
+    return audioContent;
+}
+
+function whileLoadingInterval() {
+    return setInterval(() => {
+        if (startAt) {
+            const inSec = (Date.now() - startAt) / 1000;
+            if (playWhileLoadingDuration && inSec >= playWhileLoadingDuration) {
+                playWhileLoading(playWhileLoadingDuration);
+                playWhileLoadingDuration = source.buffer.duration
+            }
+        } else if (source) {
+            playWhileLoadingDuration = source.buffer.duration;
+            startAt = Date.now();
+            playWhileLoading();
+        }
+    }, 500);
+}
+
+function playWhileLoading(duration = 0) {
+    source.connect(audioContext.destination);
+    source.connect(gainNode);
+    source.connect(analyser);
+    source.start(0, duration);
+    activeSource = source;
+}
+
--- Streaming/html/js/playButton.js
+++ Streaming/html/js/playButton.js
@@ -3,6 +3,7 @@ window.onload = () => {
     //var socket = io('http://localhost:2000');
 
     document.getElementById("knappen").onclick = async function () {
-        miscStartStream('beyonce');
+        const test = miscStartStream('beyonce');
+        test.play;
     }
 }
