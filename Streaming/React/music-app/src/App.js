52c83af4a1c60553c4efa47a4ea69c6587ee5dca
Rasmus Jensen <rasje17@student.sdu.dk>
2020-04-09 17:10:16 +0200

    react works and docker kind of works
    problem with bindmount off react not working as intended for docker compose

--- Streaming/React/music-app/src/App.js
+++ Streaming/React/music-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './39.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="head" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!
+          Learn Retract!
         </a>
       </header>
     </div>
---
+++ Streaming/React/music-app/src/Test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Test() {
+    return (
+        <div className="Test">
+            <header className= "Test-body">
+                <p>thiss is a teeeest</p>
+            </header>
+        </div>
+    );
+}
+
+export default Test;
