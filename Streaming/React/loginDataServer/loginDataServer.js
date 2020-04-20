ae15761adf55dd7d328523e71f7f5a7015c9944e
CasaRol <alexander-rol@hotmail.com>
2020-04-20 15:28:05 +0200

    Login user now goes through our postgresql database
    Login working through "./checkCred".
    minor changes in user on loginDatabaseServer as user had a username. This has been removed due to missing intend of use.

--- Streaming/React/loginDataServer/loginDataServer.js
+++ Streaming/React/loginDataServer/loginDataServer.js
@@ -8,11 +8,12 @@ const pg = require('pg');
 
 const conString = "postgresql://uzbxyxyi:j7b-g-qv6fw30KkL0dAkN1CMrPMg1sPs@balarama.db.elephantsql.com:5432/uzbxyxyi" //Can be found in the Details page
 const client = new pg.Client(conString);
+/*
 client.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
   }
-  client.query('SELECT * from users', function(err, result) {
+  client.query('SELECT (user_id, email, password) from users WHERE email = $1', ["me@mail.com"], function(err, result) {
     if(err) {
       return console.error('error running query', err);
     }
@@ -21,8 +22,9 @@ client.connect(function(err) {
     client.end();
   });
 });
+*/
 
-var users = [];
+//var users = [];
 
 const server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
@@ -32,18 +34,46 @@ server.options('*', cors());
 
 // for receiving a login request, checking whether the credentials are correct
 server.get('/checkCred', (req, res) => {
-    let foundU = users.find(u => u.email == req.query.email);
+    let askUser = new User(undefined, req.query.email, req.query.password); //user login data
+    let foundUser; //will be set when db responds
 
-    if (foundU == undefined) {
-        res.status(404).send("404 - Email does not match any users in the system!");
-    }
-    else if (foundU.password == req.query.password) {
-        res.status(200).send("200 - Login successful!");
-    } else {
-        res.status(401).send("401 - Login failed! (Probably due to wrong password, the email exists)");
-    }
+    client.connect(function (err) {
+        console.log("client connected");
+        if (err) {
+            //Cannot connect to DataBase for some reason
+            console.error('could not connect to DataBase - Try again later.', err);
+            res.status(500).send("500 - Internal Server Error");
+        }
+        client.query('SELECT * from users WHERE email = $1', [askUser.email], function (err, result) {
+            if (err) {
+                //DataBase responds with error
+                console.log("Fatal error: " + err);
+                res.status(503).send("503 - Service Unavailable (Database Error)");
+            } else if (result.rows.length == 0) {
+                //No Error but no results either
+                console.log("No match found on given email");
+                res.status(404).send("404 - Email does not exist in system");
+            }
+            //result found
+            //store result
+            foundUser = new User(result.rows[0].id, result.rows[0].email, result.rows[0].password);
 
-    res.end();
+            //Called data stored locally ending session
+            client.end();
+            console.log("client ended");
+
+            //check if email and password match
+            if (askUser.email !== foundUser.email || askUser.password !== foundUser.password) {
+                    console.log("No Match!")
+                    res.status(401).send("401 - Email or password does not match!");
+            } else if (askUser.email === foundUser.email && askUser.password === foundUser.password) {
+                console.log("User verified - Login Succesfull!")
+                res.status(200).send(foundUser.id);
+            }
+            
+        });
+    });
+
 });
 
 // for creating a new user in the system
