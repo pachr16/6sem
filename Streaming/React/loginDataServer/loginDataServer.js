c060dbf4f9f85ac684e89b57e337a1e693e0ab34
CasaRol <alexander-rol@hotmail.com>
2020-04-20 21:43:13 +0200

    Fixed database query error where we could only query once per server session #ThanksPatrick*

--- Streaming/React/loginDataServer/loginDataServer.js
+++ Streaming/React/loginDataServer/loginDataServer.js
@@ -7,24 +7,7 @@ const pg = require('pg');
 //var pg = require('pg').native
 
 const conString = "postgresql://uzbxyxyi:j7b-g-qv6fw30KkL0dAkN1CMrPMg1sPs@balarama.db.elephantsql.com:5432/uzbxyxyi" //Can be found in the Details page
-const client = new pg.Client(conString);
-/*
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT (user_id, email, password) from users WHERE email = $1', ["me@mail.com"], function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result);
-    // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
-});
-*/
 
-//var users = [];
 
 const server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
@@ -34,26 +17,33 @@ server.options('*', cors());
 
 // for receiving a login request, checking whether the credentials are correct
 server.get('/checkCred', (req, res) => {
-    let askUser = new User(undefined, req.query.email, req.query.password); //user login data
+    //Create new client
+    const client = new pg.Client(conString);
+
+    let askUser = new User(undefined, (req.query.email).toLowerCase(), req.query.password); //user login data
     let foundUser; //will be set when db responds
 
     client.connect(function (err) {
         console.log("client connected");
+
         if (err) {
             //Cannot connect to DataBase for some reason
             console.error('could not connect to DataBase - Try again later.', err);
             res.status(500).send("500 - Internal Server Error");
         }
+
         client.query('SELECT * from users WHERE email = $1', [askUser.email], function (err, result) {
             if (err) {
-                //DataBase responds with error
-                console.log("Fatal error: " + err);
+                //cannot resolve query due to unexisting table etc.
+                console.log("Fatal " + err);
                 res.status(503).send("503 - Service Unavailable (Database Error)");
+
             } else if (result.rows.length == 0) {
                 //No Error but no results either
                 console.log("No match found on given email");
                 res.status(404).send("404 - Email does not exist in system");
-            } else if (result.rows.length != 0) {
+
+            } else {
                 //store result from DB
                 foundUser = new User(result.rows[0].id, result.rows[0].email, result.rows[0].password);
 
@@ -61,16 +51,18 @@ server.get('/checkCred', (req, res) => {
                 if (askUser.email !== foundUser.email || askUser.password !== foundUser.password) {
                     console.log("No Match!")
                     res.status(401).send("401 - Email or password does not match!");
+
                 } else if (askUser.email === foundUser.email && askUser.password === foundUser.password) {
                     console.log("User verified - Login Succesfull!")
                     res.status(200).send(foundUser.id);
                 }
+                //Called data stored locally ending session
+                client.end();
+                console.log("client ended");
 
             }
 
-            //Called data stored locally ending session
-            client.end;
-            console.log("client ended");
+
 
 
 
@@ -80,8 +72,9 @@ server.get('/checkCred', (req, res) => {
 });
 
 // for creating a new user in the system
-server.get('/newUser', (req, res) => {
-    let newUser = new User(undefined, req.query.email, req.query.password); //users.find(u => u.email == req.query.email);
+server.post('/newUser', (req, res) => {
+    const client = new pg.Client(conString);
+    let newUser = new User(undefined, (req.query.email).toLowerCase(), req.query.password); //users.find(u => u.email == req.query.email);
 
     client.connect(function (err) {
         console.log("client connected");
@@ -93,7 +86,8 @@ server.get('/newUser', (req, res) => {
 
         client.query("INSERT INTO users(email, password) VALUES($1, $2)", [newUser.email, newUser.password], function (err, result) {
             if (err) {
-                if(err == 'error: duplicate key value violates unique constraint "users_email_key"') {
+                if (err == 'error: duplicate key value violates unique constraint "users_email_key"') {
+                    console.log("403 - Mail already exist");
                     res.status(403).send("403 - Mail already exist");
                 }
                 //DataBase responds with error
@@ -103,10 +97,11 @@ server.get('/newUser', (req, res) => {
                 console.log("SUCCESS!!");
                 res.status(200).send("Success! - User created!");
             }
-            client.end;
+            client.end();
             console.log("client ended");
         });
     });
+
 });
 
 server.listen(8080);
