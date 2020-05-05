071cc4764d65b2a51dae622f080955703f5b49ca
pachr16 <32641694+pachr16@users.noreply.github.com>
2020-05-05 17:45:54 +0200

    Further work with streaming using the audio element

--- Streaming/React/music-app/src/player/Player.js
+++ Streaming/React/music-app/src/player/Player.js
@@ -17,25 +17,31 @@ function Player() {
     const song_url = useSelector(state => state.song_urls[index]);
     const size = useSelector(state => state.sizes[index]);
 
-    // setup for the actual audio handling
+    // setup audio
     const audio = document.createElement('audio');
-    var mediaSource = new MediaSource();
-    var audioSourceBuffer;
-    mediaSource.addEventListener('sourceopen', function () {
-        audioSourceBuffer = mediaSource.addSourceBuffer('audio/mpeg');
-        console.log("mediaSource has indicated that it is open! " + mediaSource.readyState);
-    });
-    audio.src = URL.createObjectURL(mediaSource);
-
 
     useEffect(() => {
-
         if (currentSong != -1) {
+
+            var mediaSource = new MediaSource();
+            audio.src = URL.createObjectURL(mediaSource);
+            var audioSourceBuffer;
+            mediaSource.addEventListener('sourceopen', function () {
+                console.log("Mediasource has indicated that it is open!");
+                if (mediaSource.sourceBuffers[0] == null) {
+                    audioSourceBuffer = mediaSource.addSourceBuffer('audio/mpeg');
+                    console.log("mediaSource has added a new audiosourcebuffer! " + mediaSource.readyState);
+                }
+            });
+
+            console.log("Created new mediasource! Containing these sourcebuffers: ");
+            console.log(mediaSource.sourceBuffers);
 
             setLoading(true);   // we start loading
 
             // if there is already a song loading/playing, remove that sourcebuffer.
             if (mediaSource.activeSourceBuffers[0] != null) {
+                console.log("Removing current audiosourcebuffer to start new song");
                 mediaSource.removeSourceBuffer(mediaSource.activeSourceBuffers[0]);     // removes sourcebuffer from list, but if it still exists it's a problem - still holding the data?
             }
 
@@ -46,39 +52,40 @@ function Player() {
             // add a sourcebuffer for the new song
             //const audioSourceBuffer = mediaSource.addSourceBuffer('audio/mpeg');    // maybe need codecs
 
-            // looping over the amount of segments, starting at zero to avoid rounding issues - we should always expect to receive one not-full segment, meaning we always round up, or take 1 extra segment (the 0-indexed one).
-            var counter = 0;
-            for (let seg = 0; seg <= totalSegments; seg++) {
+            // we should always expect to receive one not-full segment, meaning we always round up, or take 1 extra segment.
+            console.log("Now fetching the first segment of song: " + title);
+            var seg = 1;
+            fetch(`${MUSIC_SERVER}/playSong?song=${song_url}&segment=${seg}`)
+                .then(function (resp) {
+                    return resp.arrayBuffer();
+                })
+                .then(function (audioSegment) {
+                    audioSourceBuffer.appendBuffer(audioSegment);       // !! NULLPOINTER WARNING !! -- audiosourcebuffer might be null if we are still loading previous song
 
-                if (counter % 5 != 0) {
-                    counter += mediaSource.duration;
-                    console.log(counter);
-                    seg--;
-                    continue;
-                }
+                    console.log("Received segment " + seg);
+                    console.log("Begin playing!");
+                    audio.play();   // when we have received the first segment, start playing
 
-                console.log("Now fetching segment: " + seg + " of song: " + title);
+                    // listening for updates to sourcebuffer, to load next segments continuously
+                    audio.addEventListener('progress', function () {
+                        console.log("We have received an onupdate event!");
+                        seg++;
 
-                fetch(`${MUSIC_SERVER}/playSong?song=${song_url}&segment=${seg}`)
-                    .then(function (resp) {
-                        return resp.arrayBuffer();
-                    })
-                    .then(function (audioSegment) {
-                        audioSourceBuffer.appendBuffer(audioSegment);       // !! NULLPOINTER WARNING !! -- audiosourcebuffer might be null if we are still loading while we switch songs
+                        fetch(`${MUSIC_SERVER}/playSong?song=${song_url}&segment=${seg}`)
+                            .then(function (resp) {
+                                return resp.arrayBuffer();
+                            })
+                            .then(function (audioSegment) {
+                                audioSourceBuffer.appendBuffer(audioSegment);
+                                console.log("Received this: " + audioSegment + " as segment " + seg);
 
-                        console.log("Received this: " + audioSegment + " as segment " + seg);
-                        if (seg == 1) {
-                            console.log("Begin playing");
-                            audio.play();   // when we have received the first segment, start playing
-                        }
-
-                        if (seg == totalSegments) {     // if we have reached the final segment, we are done loading
-                            setLoading(false);
-                        }
+                                if (seg == totalSegments + 1) {     // if we have reached the final segment, we are done loading
+                                    setLoading(false);
+                                }
+                            });
                     });
-
-            }
-
+                    console.log("Added eventListener!");
+                });
         }
     }, [currentSong])
 
