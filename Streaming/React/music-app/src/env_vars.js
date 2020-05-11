c1260d490658d601312b68d8bb023e2d60d5ebf1
Rasmus Jensen <rasje17@student.sdu.dk>
2020-05-11 16:10:24 +0200

    tls works for react app, nginx routing not updated
    the react app's development mode is served by the ngin x with tls, but the other nginx routings are not updated

--- Streaming/React/music-app/src/env_vars.js
+++ Streaming/React/music-app/src/env_vars.js
@@ -1,2 +1,7 @@
-export const LOGIN_SERVER = 'http://localhost:8080';
-export const MUSIC_SERVER = 'http://localhost:2000';
+//for when runing servers locally
+// export const LOGIN_SERVER = 'http://localhost:8080';
+// export const MUSIC_SERVER = 'http://localhost:2000';
+
+//for useing containers
+export const LOGIN_SERVER = 'https://192.168.99.100:443/login/';
+export const MUSIC_SERVER = 'https://192.168.99.100:443/music/';
